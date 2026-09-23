d7a6805e69a8316d449dd8d4e1145806c08c9edc
pthakur11205 <146504583+pthakur11205@users.noreply.github.com>
2026-09-23 11:02:17 -0400

    Add: course-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-7.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] in = new int[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] pre: prerequisites) {
+            in[pre[1]]++;
+            adj.get(pre[0]).add(pre[1]);
+        }
+        for(int i=0; i < in.length; i++) {
+            if(in[i]==0) q.offer(i);
+        }
+        
+        if(q.isEmpty()) return new int[0];
+
+        int[] res = new int[numCourses];
+        int finished=0;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            res[numCourses-finished-1] = cur;
+            for(int i: adj.get(cur)) {
+                in[i]--;
+                if(in[i]==0) q.offer(i);
+            }
+            finished++;
+        }
+
+        return (finished==numCourses) ? res: new int[0];
+    }
+}
